b7a3d308b3a2463ee1652ec09e1c325ea87f61ca
Archu21-lab <archupatil5958@gmail.com>
2026-05-27 09:25:03 +0530

    ..

--- User App/src/Component/user.jsx
+++ User App/src/Component/user.jsx
@@ -1,14 +1,26 @@
 import allUsers from "../data/user.js";
+import { useState } from "react";
 
 const User = () => {
-  return <div>
-    <h1>All Users</h1>
-    {
-        allUsers.map((User)=><div>
-            
-        </div>)
-    }
-  </div>;
+  const [users, setUsers] = useState(allUsers);
+  return (
+    <div>
+      <h1>All Users</h1>
+      {users.map((user, i) => (
+        <div key={i}>
+          <h3>{user.name}</h3>
+          <p>{user.email}</p>
+        </div>
+      ))}
+      <button
+        onClick={() => {
+          setUsers([...users, { name: "sahil", email: "sahil@yahoo.com" }]);
+        }}
+      >
+        Add
+      </button>
+    </div>
+  );
 };
 
 export default User;
