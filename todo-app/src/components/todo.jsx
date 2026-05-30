2586ea5da593c3450c2b0c2126801b4d70fe40b8
Archu21-lab <archupatil5958@gmail.com>
2026-05-30 21:36:53 +0530

    ..

--- todo-app/src/components/todo.jsx
+++ todo-app/src/components/todo.jsx
@@ -1,38 +1,62 @@
-const todos = [
-  {
-    title: "workout at 5AM",
-  },
-  {
-    title: "read books",
-  },
-  {
-    title: "see TV",
-  },
+// const todos = [
+//   {
+//     title: "workout at 5AM",
+//   },
+//   {
+//     title: "read books",
+//   },
+//   {
+//     title: "see TV",
+//   },
 
-  {
-    title: "Box Cricket",
-  },
-];
-
+//   {
+//     title: "Box Cricket",
+//   },
+// ];
+import { useState } from "react";
 const Todo = () => {
+  const [todos, setTodos] = useState([{ title: "Singing At 5AM" }]);
+  const [title, setTitle] = useState("");
   return (
     <>
       <section>
         <div className="container">
           <div className="mt-4">
             <h2>DAILY NOTES</h2>
-            <p className="fs-3 fw-bold">Structure Your Thoughts By Daily Notes..</p>
+            <p className="fs-3 fw-bold">
+              Structure Your Thoughts By Daily Notes..
+            </p>
           </div>
           <div className="d-flex my-5">
-            <input placeholder="Favourite Poet,song,Grocery,Bill etc." className="form-control"  type="text" />
-            <button className="btn btn-primary mx-2">ADD</button>
+            <input
+              placeholder="Favourite Poet,song,Grocery,Bill etc."
+              className="form-control"
+              type="text"
+              onChange={(e) => {
+                setTitle(e.target.value);
+              }} //ye onChange sirf bas input tag use krte hai tab use krte
+              // hai usme se value bahar nikalne k liye input
+              // tag mese bahar nikalne k liye means print hone k liye
+            />
+            <button
+              onClick={() => {
+                setTodos([...todos, { title }]);
+              }}
+              className={
+                "btn btn-primary mx-3 " + (title === "" ? "disabled" : "")
+              }
+            >
+              ADD
+            </button>
           </div>
 
-          {todos.map((todo) => (
-            <div>
-              <p className="container">{todo.title}</p>
-            </div>
-          ))}
+          <div className="">
+            {todos.map((todo, i) => (
+              <div key={i} className="">
+                <p className="container">{todo.title}</p>
+              </div>
+            ))}
+          </div>
         </div>
       </section>
     </>
