c3c5963fe4635202272db2bfd21522060ac44fa7
Archu21-lab <archupatil5958@gmail.com>
2026-06-04 21:13:48 +0530

    ..

--- Text-Utils/src/Components/TextUtils.jsx
+++ Text-Utils/src/Components/TextUtils.jsx
@@ -1,18 +1,31 @@
-import React from "react";
+import React, { useState } from "react";
 
 export default function TextUtils() {
+  const [text, setText] = useState("");
   return (
     <>
       <div className="container">
+        <h2 className="m-2 text-center">Text Utils</h2>
         <div className="form-floating">
           <textarea
-            className="form-control m-3"
-            placeholder="Leave a comment here"
+            onChange={(e) => {
+              setText(e.target.value);
+            }}
+            className="form-control m-3 text-black"
+            placeholder="Enter Something Amazing..."
             id="floatingTextarea2"
-            
+            style={{ height: "100px" }}
           ></textarea>
-          <label htmlFor="floatingTextarea2">Comments</label>
+          <label className="m-2" htmlFor="floatingTextarea2">
+            Content
+          </label>
         </div>
+        <hr />
+        <p>{text}</p>
+        <ul>
+          <li>Total Length:{text.length}</li>
+          <li>Total Words:{text.split(" ").length}</li>
+        </ul>
       </div>
     </>
   );
