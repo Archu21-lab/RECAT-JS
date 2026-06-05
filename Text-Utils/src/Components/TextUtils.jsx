a5a8fda7b2e74709b8d6139defda6aa8819598f6
Archu21-lab <archupatil5958@gmail.com>
2026-06-05 23:55:15 +0530

    ..

--- Text-Utils/src/Components/TextUtils.jsx
+++ Text-Utils/src/Components/TextUtils.jsx
@@ -25,6 +25,7 @@ export default function TextUtils() {
         <ul>
           <li>Total Length:{text.length}</li>
           <li>Total Words:{text.split(" ").length}</li>
+          <li>Reading Speed:</li>
         </ul>
       </div>
     </>
