b7ad111997d8bee6a1f178292777c1ea0ace2906
Archu21-lab <archupatil5958@gmail.com>
2026-06-18 15:15:12 +0530

    ..

--- routing-app/src/App.jsx
+++ routing-app/src/App.jsx
@@ -4,10 +4,17 @@ import Home from "./screens/Home";
 import Contact from "./screens/Contact";
 import About from "./screens/About";
 import Profile from "./screens/Profile";
+import { Link} from "react-router";
 
 export default function App() {
   return (
     <>
+      <nav>
+        <Link to={"/"}> Logo </Link>
+        <Link to={"/profile"}> Profile </Link>
+        <Link to={"/about"}>About</Link>
+        <Link to={"/contact"}>Contact</Link>
+      </nav>
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/contact" element={<Contact />} />
@@ -17,3 +24,9 @@ export default function App() {
     </>
   );
 }
+
+//last in first out->LIFO concept
+//single page application
+//portfolio website->figma community
+
+//click -> check validate -> navigate
--- routing-app/src/screens/Home.jsx
+++ routing-app/src/screens/Home.jsx
@@ -1,7 +1,43 @@
-import React from 'react'
+import React, { useState } from "react";
+import { useNavigate  } from "react-router";
+
+const realEmail="admin123@gmail.com"
+const realPassword="123456"
 
 export default function Home() {
+  const navigate = useNavigate();
+
+  const[email,setEmail]=useState();
+  const [password,setPassword]=useState();
   return (
-    <div>Home</div>
-  )
+    <>
+      <div>
+        <input onChange={(e)=>{
+            setEmail(e.target.value)
+        }} type="text" placeholder="Enter Email" />
+      </div>
+      <div>
+        <input onChange={(e)=>{
+            setPassword(e.target.value)
+        }} type="text" placeholder="Enter Password" />
+      </div>
+      <div>
+        <button onClick={()=>{
+         if(realEmail==email && realPassword==password){
+           navigate("/about");
+         }else{
+            alert("Your Email or Password Wrong !")
+         }
+        }}>Login</button>
+      </div>
+    </>
+  );
 }
+
+//login authenticated (valid - user enter == records )
+//private routing 
+//protected routes = if login then about
+
+//before login page with portfolio..
+//UI AI
+
