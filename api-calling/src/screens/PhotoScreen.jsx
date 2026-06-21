2224c68e54b1f581e6a62eaab0125deaed73a6e3
Archu21-lab <archupatil5958@gmail.com>
2026-06-21 13:57:58 +0530

    ..

--- api-calling/src/screens/PhotoScreen.jsx
+++ api-calling/src/screens/PhotoScreen.jsx
@@ -1,12 +1,29 @@
 import React, { useState, useEffect } from "react";
+import axios from "axios";
 
 export default function PhotoScreen() {
   const [photos, setPhotos] = useState([]);
-  const [page, SetPage] = useState({ start: 0, end: 9, page: 1 });
+  const [page, SetPage] = useState({ start: 0, end: 9 });
+  // const fetchPhotos = async () => {
+  //   const res = await fetch("https://jsonplaceholder.typicode.com/photos");
+  //   const data = await res.json();
+  //   setPhotos(data);
+  // };
   const fetchPhotos = async () => {
-    const res = await fetch("https://jsonplaceholder.typicode.com/photos");
-    const data = await res.json();
-    setPhotos(data);
+    const res = await axios.get("https://jsonplaceholder.typicode.com/photos");
+    setPhotos(res.data);
+  };
+
+  const handlePagination = (x) => {
+    let page;
+    if (x == "") {
+      page = 1;
+    } else {
+      page = Number(x);
+    }
+    const start = (page - 1) * 10;
+    const end = start + 9;
+    SetPage({ ...page, start, end });
   };
 
   useEffect(() => {
@@ -20,7 +37,10 @@ export default function PhotoScreen() {
           if (i >= page.start && i <= page.end) {
             return (
               <div key={i}>
-                <p>{photo.title}</p>
+                <p>
+                  {i + 1}
+                  {photo.title}
+                </p>
               </div>
             );
           }
@@ -45,6 +65,36 @@ export default function PhotoScreen() {
       >
         Next++
       </button>
+      <div>
+        <ul className="pagination">
+          <li className="page-item">
+            <a className="page-link" href="#">
+              Previous
+            </a>
+          </li>
+          <li onClick={() => handlePagination(1)} className="page-item">
+            <a className="page-link" href="#">
+              1
+            </a>
+          </li>
+          <li onClick={() => handlePagination(2)} className="page-item">
+            <a className="page-link" href="#">
+              2
+            </a>
+          </li>
+          <li onClick={() => handlePagination(3)} className="page-item">
+            <a className="page-link" href="#">
+              3
+            </a>
+          </li>
+          <li className="page-item">
+            <a className="page-link" href="#">
+              Next
+            </a>
+          </li>
+        </ul>
+      </div>
+      {/* <input onChange={handlePagination} type="text" /> */}
     </div>
   );
 }
