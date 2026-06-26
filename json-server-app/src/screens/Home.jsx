28b3e39642666e8a832852aa48520b70b362f08e
Archu21-lab <archupatil5958@gmail.com>
2026-06-27 00:06:21 +0530

    ..

--- json-server-app/src/screens/Home.jsx
+++ json-server-app/src/screens/Home.jsx
@@ -3,15 +3,19 @@ import axios from "axios";
 
 export default function Home() {
   const [users, setUsers] = useState([]);
-  const [data,setData]=useState({});
-  
-    const getName =(e)=>setData({...data,name:e.target.value})
-    const getRole=(e)=>setData({...data,role:e.target.value})
+  const [data, setData] = useState({});
 
-    const handleSetUsers= async ()=>{
-        const res = await axios.post("http://localhost:3000/users",data)
-    }
-    
+  const getName = (e) => setData({ ...data, name: e.target.value });
+  const getRole = (e) => setData({ ...data, role: e.target.value });
+
+  const handleSetUsers = async () => {
+    const res = await axios.post("http://localhost:3000/users", data);
+   if(res.status==201){
+    alert("users Inserted Successfully !")
+   }else{
+    alert("can't Add User !")
+   }
+  };
 
   const handleFetchUsers = async () => {
     const res = await axios.get("http://localhost:3000/users");
@@ -24,17 +28,24 @@ export default function Home() {
 
   return (
     <div>
-        <input  onChange={getName} type="text" placeholder="Enter Nmae"/>
-        <input onChange={getRole} type="text" placeholder="Enter Role"/>
-        <button onClick={}>Submit</button>
+      <input onChange={getName} type="text" placeholder="Enter Name" />
+      <input onChange={getRole} type="text" placeholder="Enter Role" />
+      <button
+        onClick={async() => {
+        await  handleSetUsers();
+         await handleFetchUsers();
+        }}
+      >
+        Submit
+      </button>
       <div>
         {users.length == 0 ? (
           <p>No Users</p>
         ) : (
           users.map((user, i) => {
-            <div key={i}>
+           return( <div key={i}>
               <p>{user.name}</p>
-            </div>;
+            </div>)
           })
         )}
       </div>
