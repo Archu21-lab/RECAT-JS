3733f0bd406c10769e232208ac39c7f7a5c35d91
Archu21-lab <archupatil5958@gmail.com>
2026-07-02 17:08:57 +0530

    ..

---
+++ context-project/src/screens/Home.jsx
@@ -0,0 +1,14 @@
+import React ,{useContext}from 'react'
+import { TodoContext } from '../context/TodoProvider'
+
+export default function Home() {
+    const {count,increment,decrement} = useContext(TodoContext);
+  return (
+    <div>
+        <h1>{count}</h1>
+        <button onClick={increment}>++</button>
+        <button onClick={decrement}>--</button>
+
+    </div>
+  )
+}
