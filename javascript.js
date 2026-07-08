0cbe40ff5adb87ab4e03e5bcb7018df0957c0b12
Archu21-lab <archupatil5958@gmail.com>
2026-07-08 20:42:52 +0530

    ..

---
+++ javascript.js
@@ -0,0 +1,24 @@
+//primitive
+
+// 7 types:String,Number,Boolean,null,undefined,Symbol,BigInt
+
+//Reference type : non-primitive
+ // Array,Objects,Functions
+
+ //memory
+
+ //stack memory  mein (primitive) k data type use hote hain
+ // aur heap memory  mein (non-premitive) k data type use hote hain
+
+// jab hum variable mein value ko change krte hain to vo change ho jati hain 
+//example 
+// let a=10;
+//a=b;
+//b=20;
+//console.log(a);
+//console.log(b);
+// to isme result a=10 
+//b=20; result aayega 
+//aur jab hum heap memory mein store krte hain tab uski value just like example 
+//const fun = Function (){ const a=10 ; a=b b=20; to consolo kr kr print krenge to uski value mein
+// result jo last mein change kiya vahi aayega console.log(a) console.log(b) dono mein 20 hi print hoga }
