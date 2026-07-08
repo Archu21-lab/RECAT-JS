3559521b968778274694b7caef43db60e0c3db3e
Archu21-lab <archupatil5958@gmail.com>
2026-07-08 21:40:37 +0530

    ..

--- javascript.js
+++ javascript.js
@@ -22,3 +22,26 @@
 //aur jab hum heap memory mein store krte hain tab uski value just like example 
 //const fun = Function (){ const a=10 ; a=b b=20; to consolo kr kr print krenge to uski value mein
 // result jo last mein change kiya vahi aayega console.log(a) console.log(b) dono mein 20 hi print hoga }
+//array ki method mein 
+//const getName= new String ('archana')
+//to usme  r letter hain vo kitne number pe hain vo check krne k liye indexOf 
+//console.log (getName.indexOf('t'));
+// to usme ye jo number hain us position pe konsa latter hain vo check krne k liye
+// console.log(getName.charAt(2));
+//archana
+
+// sub string mein hum value (0,4) tak k print krte hain 
+//console.log (arch) itna hi print hoga 
+
+//slice mein hum negative value bi print kr skte hain means jo reverse latter bhi print kr skte hain 
+//trim mein latter k starting aur ending sapce ko bhi remove kr deta hain 
+
+//const url = "https://archu.com/archu%20patil"
+// by default %20 hain to agar usko replace krna hain to usko replace method use hoti hain
+
+//console.log(url.replace('%20' ,'-'))
+
+// includes  se hum us url  mein vo name hain ki nahi vo check krte hain hoga to true nhi hoga to false
+
+// console.log(my name is  ${archana } my education in ${b.tech})
+// new codeing mein hum variable likhne k liye + use krte hain but modern way mein ${} mein likhna chhahiye 
